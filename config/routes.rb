a250b1a4b15c3c803bcdc1ec0386241730ea1cd6
Anders Tunold Kråkenes <tunold@gmail.com>
2016-09-19 18:03:10 -0400

    Good stuff!

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'user#index'
 
-  resources :users, only: [:index, :new, :create]
+  resources :users, only: [:index, :new, :edit, :create, :destroy]
   resources :projects, only: [:index, :new, :create, :edit, :destroy]
-  resources :votes, only: [:index]
-  resources :rankings, only: [:index]
+  resources :votes, only: [:index, :new, :create]
+  resources :rankings, only: [:index, :new, :create]
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
